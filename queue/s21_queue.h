7fc7a39dbf7214eab7225b137d5236b96b5a6217
OddEer0 <oddeer0@gmail.com>
2024-04-09 00:49:59 +0300

    add queue stack classes

---
+++ queue/s21_queue.h
@@ -0,0 +1,37 @@
+#ifndef S21_CONTAINERS_QUEUE_H_
+#define S21_CONTAINERS_QUEUE_H_
+
+#include "../list/s21_list.h"
+
+namespace s21 {
+    template <class T, class Container = s21::list<T>>
+    class queue {
+    public:
+        using reference = Container::reference;
+        using const_reference = Container::const_reference;
+        using size_type = Container::size_type;
+        using value_type = Container::value_type;
+
+    private:
+        Container container_;
+
+    public:
+        stack();
+        ~stack();
+
+        reference top();
+        const_reference top();
+
+        bool empty();
+        size_type size();
+
+        void push(const value_type& value);
+        void push(value_type&& value);
+        void pop();
+        template<class R>
+        void push_range(R&& rg);
+        void swap (stack & Other);
+    };
+}
+
+#endif
